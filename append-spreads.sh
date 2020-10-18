b2eaffa86b3b931ff1f992061512aa69a990a97e
Jaegrqualm <rwhutcherson@gmail.com>
2020-10-18 16:21:10 -0500

    Add files via upload

---
+++ append-spreads.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+
+for i in $(seq -f "%02g" $1 2 $2 ) # first, iterate, last. last is the first page of the last spread. first and last could potentially be input $1 and $2 instead of hardcoded
+do
+	a=$((10#$i +1)) #the 10# is to force decimal mode, so as to not get an octal error. add one to the (zero padded) intake value and use it as part of a filename in the convert command
+	convert $(printf "%02g" "$a").png $i.png -strip +append dump/$i-$(printf "%02g" "$a").png # printf re-adds the zero padding to the other filename number. -strip removes png metadata(?) that causes consecutive runs on the same files to produce results with different checksums.
+done
